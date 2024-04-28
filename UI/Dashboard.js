0b826f97323d67566174707993e7ebbfbc4e3c00
Brenda Vega Contreras <bvega96@outlook.com>
2024-04-28 02:45:44 -0700

    Dashboard update

--- UI/Dashboard.js
+++ UI/Dashboard.js
@@ -1,9 +1,39 @@
-import { View, Text, TextInput, Button, StyleSheet, Image, ScrollView, Dimensions, ImageBackground } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Image, ScrollView, Dimensions, ImageBackground,TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+import { FlatList } from 'react-native-web';
 import styles from "./CommonStyleSheet"
 import MyImage from '../assets/bgimages/dashboard.jpg';
 //show saved stories
 
 export default function Dashboard({ navigation }) {
+	[storyTitles, setListTiles] = useState([]);
+	const remove = async () => {
+		try {
+		  await AsyncStorage.removeItem("Test Title"); // TODO: match the current list of stories
+		} catch(err) {
+		  alert(err);
+		} /*finally {
+		  setName("");
+		}*/
+	  };
+	  
+	  const refreshDashboard = async () => {
+		try {
+			let titlesJSONString = await AsyncStorage.getItem("titles");
+			// If no titles saved yet, initialize an empty array
+			let titlesArray = titlesJSONString ? JSON.parse(titlesJSONString) : [];
+			setListTiles(titlesArray);
+
+		} catch(err) {
+		  alert(err);
+		}
+	  };
+	 
+	  useEffect(()=>{
+		//refreshDashboard();
+	  }, []);
+
 	return (
 		<ImageBackground source={MyImage} style={styles.backgroundImage}>
 		<View style={styles.container}>
@@ -18,7 +48,16 @@ export default function Dashboard({ navigation }) {
 				title="Go to Read Story Page"
 				onPress={() => navigation.navigate('ReadStory')}
 			/>
+			<TouchableOpacity style={styles.buttonStyle1} onPress={() => remove()}>
+				<Text style={{ color: "white" }}>Remove my story!</Text>
+			</TouchableOpacity>
 		</View>
 		</ImageBackground>
 	);
 }
+
+
+//import { View, Text, TextInput, Button, StyleSheet, Image, ScrollView, Dimensions,  } from 'react-native';
+
+
+//show saved stories
