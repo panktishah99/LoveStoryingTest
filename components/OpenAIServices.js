822de76819bd20cf69deab491140052cadc5baea
Brenda Vega Contreras <bvega96@outlook.com>
2024-04-28 04:32:03 -0700

    Test1

--- components/OpenAIServices.js
+++ components/OpenAIServices.js
@@ -44,6 +44,50 @@ export function imageGeneration(textPrompt, numImg) {
 */
 
 
+
+export function textCompletion(inputText, selectedMaxResTokenLength,selectedTemperature, selectedTopP, selectedFreqPenalty, selectedPresPenalty, selectedDataModel) {
+
+	const localUrl = "https://openaiserver.loca.lt/openai/completions/3.0"; // public endpoint for text completion
+
+    return fetch(localUrl, { // Use localUrl instead of url
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "prompt": inputText,
+            "max_tokens": selectedMaxResTokenLength,
+            "temperature": selectedTemperature,
+            "top_p": selectedTopP,
+            "frequency_penalty": selectedFreqPenalty,
+            "presence_penalty": selectedPresPenalty,
+            "model": selectedDataModel
+        })
+    });
+}
+
+
+
+export function imageGeneration(textPrompt, numImg) {
+  
+	const localUrl = "https://openaiserver.loca.lt/openai/generations";// public endpoint for img
+
+    return fetch(localUrl, { // Use localUrl instead of url
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "prompt": textPrompt,
+            "n" : numImg
+        })
+    });
+}
+
+
+/*
 // This part is for the mocking openAI API calling
 export function textCompletion(inputText, selectedMaxResTokenLength,selectedTemperature, selectedTopP, selectedFreqPenalty, selectedPresPenalty, selectedDataModel) {
     // Simulating receiving data without sending a request
@@ -75,7 +119,7 @@ export function imageGeneration(textPrompt) {
     // Simulate resolving a promise with the mock response
     return Promise.resolve(mockResponse); //=================
 }
-
+*/
 export function titleGeneration(textPrompt) {
     // Simulating receiving data without sending a request
     const mockResponse =  {
